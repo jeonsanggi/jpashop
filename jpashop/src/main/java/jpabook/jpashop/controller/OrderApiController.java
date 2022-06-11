547b9e63d79f31445cf62e89dab828e5bf5cd828
jeonsanggi <jsg2861@gmail.com>
2022-06-11 21:38:03 +0900

    feat - 주문 조회V4(JPA에서 DTO 직접 조회)

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderApiController.java
@@ -9,6 +9,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.Query.OrderQueryDto;
+import jpabook.jpashop.repository.Query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb.OrderDto;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -64,6 +67,12 @@ public class OrderApiController {
             .collect(Collectors.toList());
         return collect;
     }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
 
     @Getter
     static class OrderDto {
